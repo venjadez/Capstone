6595bdfbecf0b8ab43f4d0fa124ff2401cb4ffb8
venjadez <stevenjamesmendez18@gmail.com>
2022-12-20 09:04:24 +0800

    other details added

--- resources/views/Jersey.blade.php
+++ resources/views/Jersey.blade.php
@@ -19,24 +19,24 @@
 			<div class="card">
 				<div class="card-header"><h2>Enter Jersey Details</h2></div>
 				<div class="card-body"><div class="input-group mb-3">
-      <span class="input-group-text">DATE DUE:</span>
-      <input type="date" class="form-control" name="DateDue">
+      <span class="input-group-text" required>DATE DUE:</span>
+      <input type="date" class="form-control" name="DateDue" required>
     </div><div class="input-group mb-3">
       <span class="input-group-text">TEAM:</span>
-      <input type="Text" placeholder="ex. Los Angeles Lakers"class="form-control" name="TEAM">
+      <input type="Text" placeholder="ex. Los Angeles Lakers"class="form-control" name="TEAM" required>
     </div><div class="input-group mb-3">
       <span class="input-group-text">MANAGER:</span>
-      <input type="Text" class="form-control" name="MANAGER"></div>
+      <input type="Text" class="form-control" name="MANAGER" required></div>
       <div class="input-group mb-3">
       <span class="input-group-text">CONTACT PERSON:</span>
-      <input type="text" class="form-control" name="CONTACTPERSON"></div>
+      <input type="text" class="form-control" name="CONTACTPERSON" required></div>
       <div class="input-group mb-3">
       <span class="input-group-text">CONTACT NUMBER</span>
-      <input type="tel" class="form-control" name="CONTACTNUMBER" placeholder="ex. 09XXXXXXXXX"></div>
+      <input type="tel" class="form-control" name="CONTACTNUMBER" placeholder="ex. 09XXXXXXXXX" required></div>
 
 						<div class="table-repsonsive">
 							<span id="error"></span>
-							<table class="table table-bordered" id="item_table">
+							<table class="table" id="item_table">
 								<tr>
 								<th>NO.</th>
         <th>NAME</th>
@@ -46,10 +46,28 @@
         <th><button type="button" class="btn btn-success btn-sm add">Add Field</button></th>
 								</tr>
 							</table>
+							<div class="input-group mb-3">
+							<span class="input-group-text">LAYOUT/DESIGN:</span>
+      <input type="file" class="form-control" name="LAYOUT/DESIGN" multiple></div>
+	  <div class="input-group mb-3">
+		<span class="input-group-text">MUSE:</span>
+		<select class="form-control selectpicker" required><option value="">Nothing selected</option><option value="dress">DRESS</option><option value="sando">SANDO</option></select>
+		<input type="number" class="form-control muse_number" name="muse_number[]" placeholder="Pick a number">
+		<select class="form-control selectpicker" name="muse_top_sizes[]"><option value="">Select size</option><option value ="XS">Extra Small</option><option value ="S">Small</option><option value ="Medium">Medium</option><option value ="L">Large</option><option value = "XL">Extra Large</option><option value ="2XL">2XLarge</option><option value ="3XL">3XLarge</option><option value ="4XL">4XLarge</option><option value ="5XL">5XLarge</option></select></div>
+		<div class="input-group mb-3">
+			<span class="input-group-text">MANAGER:</span>
+			<select class="form-control selectpicker" required><option value="">Nothing selected</option><option value="dress">DRESS</option><option value="sando">SANDO</option></select>
+			<input type="number" class="form-control manager_number" name="manager_number[]" placeholder="Pick a number">
+			<select class="form-control selectpicker" name="manager_top_sizes[]" required><option value="">Select size</option><option value ="XS">Extra Small</option><option value ="S">Small</option><option value ="Medium">Medium</option><option value ="L">Large</option><option value = "XL">Extra Large</option><option value ="2XL">2XLarge</option><option value ="3XL">3XLarge</option><option value ="4XL">4XLarge</option><option value ="5XL">5XLarge</option></select></div>
+	  <div class="input-group mb-3">
+		<span class="input-group-text">OTHER DETAILS:</span>
+		<input type="Text" class="form-control" name="OTHERDETAILS" placeholder="Add Customization" required></div>
+
 							<div align="center">
 								<input type="submit" name="submit" id="submit_button" class="btn btn-primary" value="Submit" />
 							</div>
 						</div>
+                       
 					</form>
 					
 				</div>
@@ -72,8 +90,8 @@ $(document).ready(function(){
       html += '<td>'+no+'</td>';
       html += '<td><input type="text" class="form-control jersey_name" name="jersey_name[]"></td>';
       html += '<td><input type="number" class="form-control jersey_number" name="jersey_number[]"></td>';
-      html += '<td><select class="form-control selectpicker" name="jersey_top_sizes[]"><option value="">Select size</option><option value ="XS">Extra Small</option><option value ="S">Small</option><option value ="Medium">Medium</option><option value ="L">Large</option><option value = "XL">Extra Large</option><option value ="2XL">2XLarge</option><option value ="3XL">3XLarge</option><option value ="4XL">4XLarge</option><option value ="5XL">5XLarge</option></select></td>';
-      html += '<td><select class="form-control selectpicker" name="jersey_short_sizes[]"><option value="">Select size</option><option value ="XS">Extra Small</option><option value ="S">Small</option><option value ="Medium">Medium</option><option value ="L">Large</option><option value = "XL">Extra Large</option><option value ="2XL">2XLarge</option><option value ="3XL">3XLarge</option><option value ="4XL">4XLarge</option><option value ="5XL">5XLarge</option></select></td>';
+      html += '<td><select class="form-control selectpicker" name="jersey_top_sizes[]" required><option value="">Select size</option><option value ="XS">Extra Small</option><option value ="S">Small</option><option value ="Medium">Medium</option><option value ="L">Large</option><option value = "XL">Extra Large</option><option value ="2XL">2XLarge</option><option value ="3XL">3XLarge</option><option value ="4XL">4XLarge</option><option value ="5XL">5XLarge</option></select></td>';
+      html += '<td><select class="form-control selectpicker" name="jersey_short_sizes[]" required><option value="">Select size</option><option value ="XS">Extra Small</option><option value ="S">Small</option><option value ="Medium">Medium</option><option value ="L">Large</option><option value = "XL">Extra Large</option><option value ="2XL">2XLarge</option><option value ="3XL">3XLarge</option><option value ="4XL">4XLarge</option><option value ="5XL">5XLarge</option></select></td>';
 
 		var remove_button = '';
 		no++;
